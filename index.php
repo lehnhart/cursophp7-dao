bef4975759aaaf7ccc69b9f8c24337e2f5a9e980
lehnhart <lehnhart@gmail.com>
2019-03-01 11:31:55 -0300

    Métodos de lista

--- index.php
+++ index.php
@@ -2,6 +2,27 @@
 
 require_once ("config.php");
 
-$root = new Usuario();
-$root->loadById(4);
-echo $root;
+// -> Carrega um usuário
+
+//$root = new Usuario();
+//$root->loadById(4);
+//echo $root;
+
+
+// -> Carrega uma lista de usuários
+
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+
+// -> Carrega uma lista de usuários buscando pelo login. Neste caso, logins que contenham "ta" no nome.
+
+//$search = Usuario::search("ta");
+//echo json_encode($search);
+
+
+// -> Carrega um usuário usando login e senha
+
+$usuario = new Usuario();
+$usuario->login("eliete", "12345");
+echo $usuario;

c19418b0618d77ae289f39b6b5edd8db00d5a920
lehnhart <lehnhart@gmail.com>
2019-03-01 15:22:44 -0300

    Inserir e alterar

--- index.php
+++ index.php
@@ -17,12 +17,29 @@ require_once ("config.php");
 
 // -> Carrega uma lista de usuários buscando pelo login. Neste caso, logins que contenham "ta" no nome.
 
-//$search = Usuario::search("ta");
+//$search = Usuario::search("al");
 //echo json_encode($search);
 
 
 // -> Carrega um usuário usando login e senha
 
+//$usuario = new Usuario();
+//$usuario->login("eliete", "12345");
+//echo $usuario;
+
+
+// -> Adicionando um novo usuário usando função insert
+
+//$aluno = new Usuario("aluno", "54321");
+//$aluno->insert();
+
+//echo $aluno;
+
+
+// -> Fazendo um Update do usuário
+
 $usuario = new Usuario();
-$usuario->login("eliete", "12345");
+$usuario->loadById(9);
+$usuario->update("professor", "123456");
 echo $usuario;
+

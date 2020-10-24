c2afc98ef69d2ef253426287968ca42ec64d5d91
Rafael Lehnhart <rafael@MacBook-Pro-de-Rafael.local>
2020-10-24 17:11:24 -0300

    Retomando o projeto do curso.

--- class/Sql.php
+++ class/Sql.php
@@ -1,37 +1,32 @@
 <?php
 
-class Sql extends PDO
-{
+class Sql extends PDO {
+    
     private $conn;
 
-    public function __construct()
-    {
+    public function __construct() {
         $this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "rafa81");
     }
 
-    public function setParams($statement, $parameters = array())
-    {
-        foreach ($parameters as $key => $value)
-        {
-            $this->setParam($statement, $key, $value);
+    public function setParams($statment, $parameters = array()) {
+        
+        foreach ($parameters as $key => $value) {
+            $this->setParam($statment, $key, $value);
         }
     }
 
-    private function setParam($statement, $key, $value)
-    {
-        $statement->bindParam($key, $value);
+    private function setParam($statment, $key, $value) {
+        $statment->bindParam($key, $value);
     }
 
-    public function query($rawQuery, $params = array())
-    {
+    public function query($rawQuery, $params = array()) {
         $stmt = $this->conn->prepare($rawQuery);
         $this->setParams($stmt, $params);
         $stmt->execute();
         return $stmt;
     }
 
-    public function select($rawQuery, $params = array()):array
-    {
+    public function select($rawQuery, $params = array()):array {
         $stmt = $this->query($rawQuery, $params);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
